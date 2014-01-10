b490cb9ef17bf5c28c53708d399a2f8fb713f7b0
Tim Layton <laytontm@gmail.com>
2014-01-10 05:46:36 -0600

    test http service

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,15 +2,25 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('agfdApp.controllers'));
+describe('PillarsCtrl', function(){
+	var scope, ctrl, $httpBackend;
 
+  beforeEach(module('agfdApp'));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+  	$httpBackend = _$httpBackend_;
+  	$httpBackend.expectGET('model/pillars.json').
+  		respond([{name: 'Taste'}, {name: 'Spirit'}]);
+ 
+      scope = $rootScope.$new();
+      ctrl = $controller('PillarsCtrl', {$scope: scope});
+    }));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+  it('should create "pillars" model with 2 pillars fetched from xhr', function() {
+	  expect(scope.pillars).toBeUndefined();
+	  $httpBackend.flush();
+	 
+	  expect(scope.pillars).toEqual([{name: 'Taste'},{name: 'Spirit'}]);
+	});
+
 });
