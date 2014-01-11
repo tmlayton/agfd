6af24d66d307510bfde84061ff5d940afc4bb545
Tim Layton <laytontm@gmail.com>
2014-01-11 03:30:11 -0600

    added script to start node.js web server, start unit tests and compass watch

---
+++ scripts/node.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /Users/Tim/Sites/agfd3
+node scripts/web-server.js
---
+++ scripts/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /Users/Tim/Sites/agfd3
+open -a Terminal.app /Users/Tim/Sites/agfd3/scripts/test.sh
+open -a Terminal.app /Users/Tim/Sites/agfd3/scripts/node.sh
+compass watch
