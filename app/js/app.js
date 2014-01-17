d4dddcb05d99df1c8aad3090d23e816f3ac3218b
Tim Layton <laytontm@gmail.com>
2014-01-17 03:15:56 -0600

    added routeProvider for new views

--- app/js/app.js
+++ app/js/app.js
@@ -12,5 +12,9 @@ angular.module('agfdApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/pillars.html', controller: 'AgfdCtrl'});
   $routeProvider.when('/sign-up', {templateUrl: 'partials/sign-up.html', controller: 'AgfdCtrl'});
+  $routeProvider.when('/photo', {templateUrl: 'partials/photo.html', controller: 'AgfdCtrl'});
+  $routeProvider.when('/photo-edit', {templateUrl: 'partials/photo-edit.html', controller: 'AgfdCtrl'});
+  $routeProvider.when('/faq', {templateUrl: 'partials/faq.html', controller: 'AgfdCtrl'});
+  $routeProvider.when('/privacy', {templateUrl: 'partials/privacy.html', controller: 'AgfdCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
