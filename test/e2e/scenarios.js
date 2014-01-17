f91ea9ae343b419f41ab269759a6b6ba5f0877b7
Tim Layton <laytontm@gmail.com>
2014-01-17 03:34:55 -0600

    updated e2e tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -34,8 +34,28 @@ describe('agfd app', function() {
       browser().navigateTo('/app/index.html#/sign-up');
     });
 
+    it('should only display 4 pillars', function() {
+       expect(repeater('[ng-view] .selected-pillars li').count()).toBe(4);
+    });
+
     it('should render sign-up when user navigates to /sign-up', function() {
       expect(element('[ng-view] .btn-facebook').text()).toMatch('Sign Up Privately with Facebook');
+    });
+
+  });
+
+  describe('photo', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('/app/index.html#/photo');
+    });
+
+    it('should render a random sample post', function() {
+      expect(element('[ng-view] .sample .quote').text()).not().toMatch('""');
+    });
+
+     it('should render a random sample post and filter a single pillar', function() {
+      expect(repeater('[ng-view] .sample i').count()).toBe(1);
     });
 
   });
