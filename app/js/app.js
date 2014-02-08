aa60910796ec2db1baf7fff6830cd5683658fbfb
Tim Layton <laytontm@gmail.com>
2014-02-08 03:10:24 -0600

    added routes for new pages

--- app/js/app.js
+++ app/js/app.js
@@ -17,11 +17,19 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/photo', {templateUrl: 'partials/sign-up/photo.html', controller: 'AgfdCtrl'});
   $routeProvider.when('/photo-edit', {templateUrl: 'partials/sign-up/photo-edit.html', controller: 'AgfdCtrl'});
 
+  //log in
+  $routeProvider.when('/log-in', {templateUrl: 'partials/log-in.html', controller: 'AgfdCtrl'});
+
   //profile
   $routeProvider.when('/profile', {templateUrl: 'partials/profile/profile.html', controller: 'AgfdCtrl'});
 
   //pages
+  $routeProvider.when('/blog', {templateUrl: 'partials/pages/blog.html', controller: 'AgfdCtrl'});
   $routeProvider.when('/faq', {templateUrl: 'partials/pages/faq.html', controller: 'AgfdCtrl'});
+  $routeProvider.when('/about', {templateUrl: 'partials/pages/about.html', controller: 'AgfdCtrl'});
+  $routeProvider.when('/careers', {templateUrl: 'partials/pages/careers.html', controller: 'AgfdCtrl'});
+  $routeProvider.when('/contact', {templateUrl: 'partials/pages/contact.html', controller: 'AgfdCtrl'});
   $routeProvider.when('/privacy', {templateUrl: 'partials/pages/privacy.html', controller: 'AgfdCtrl'});
+  $routeProvider.when('/terms', {templateUrl: 'partials/pages/terms.html', controller: 'AgfdCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
