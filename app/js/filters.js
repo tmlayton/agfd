a566e165a9bfe7bef9e26ac1d78b0b978165c3d4
Tim Layton <laytontm@gmail.com>
2014-02-14 02:59:49 -0600

    removed abbr filter, added truncate filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -19,8 +19,19 @@ agfdApp.filter('range', function() {
   };
 });
 
-agfdApp.filter('abbr', function () {
-	return function(input) {
-		return input.substr(0,1) + ".";
+agfdApp.filter('truncate', function() {
+  return function(text, length, end) {
+    if (isNaN(length))
+      length = 10;
+
+    if (end === undefined)
+      end = "...";
+
+    if (text.length <= length || text.length - end.length <= length) {
+      return text;
+    }
+    else {
+      return String(text).substring(0, length-end.length) + end;
+    }
   };
 });
