34a4db8ffb810b812dbefaa261c1896576aab627
Tim Layton <laytontm@gmail.com>
2016-02-23 20:07:58 -0500

    remove source files, move app to root

--- scripts/node.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /Users/Tim/Sites/agfd3
-node scripts/web-server.js
--- scripts/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /Users/Tim/Sites/agfd3
-open -a Terminal.app /Users/Tim/Sites/agfd3/scripts/test.sh
-open -a Terminal.app /Users/Tim/Sites/agfd3/scripts/node.sh
-compass watch
